d78033d6109c476984aa14736da34d2706557c41
cordonezjolon <cordonezjolon@gmail.com>
2023-03-12 15:25:05 -0600

    Eliminar llaves de configuracion

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 const login  =  async(req , res = response) => {
@@ -51,6 +52,28 @@ const login  =  async(req , res = response) => {
 
 }
 
+const googleSingIn = async(req, res = response) => {
+    const {id_token} = req.body;
+
+
+    try {
+        const {nombre,img,correo} = await googleVerify(id_token);
+
+        res.json({
+                msg: 'Todo bien!',
+                id_token
+        })
+    } catch (error) {
+        json.status(400).json({
+            ok:false
+            ,msg:'El token no se pudo verificar.'
+        })
+        
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
