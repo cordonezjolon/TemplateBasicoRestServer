09b6159a7b3181f6078a15a8f0be3a7e94f9c838
cordonezjolon <cordonezjolon@gmail.com>
2023-04-11 22:21:00 -0600

    Fin seccion 13 - version 6.0.0 Integracion de servicios de nube para imagenes

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
 const { subirArchivo } = require('../helpers');
 const { Usuario, Producto } = require('../models');
 const { response } = require('express');    
@@ -99,8 +101,68 @@ const monstrarImagen = async (req, res = response) => {
     res.sendFile(path.join(__dirname, '../assets/noImage.jpg'));
 }
 
+
+const actualizarImagenCloudinary = async (req, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if(!modelo){
+                return res.status(400).json({msg: `No existe un usuario con el id ${id}`});
+            }
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if(!modelo){
+                return res.status(400).json({msg: `No existe un producto con el id ${id}`});
+            }
+            break;
+        default:
+            return res.status(500).json({msg: 'Se me olvido validar esto'});
+    }
+
+    //Limpiar imagenes previas
+    if(modelo.img){
+        console.log(modelo.img);
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[nombreArr.length - 1];
+        const [public_id] = nombre.split('.');
+        console.log(public_id);
+        cloudinary.uploader.destroy(public_id);
+        
+    }
+
+    const {tempFilePath} = req.files.archivo;
+
+    cloudinary.uploader.upload(tempFilePath, (err, result) => {
+        if(err){
+            return res.status(400).json({msg: 'Error al subir la imagen'});
+        }
+        modelo.img = result.secure_url;
+        modelo.save();
+        res.json(
+            modelo
+        );
+    });
+
+
+    /*const nombre = await subirArchivo(req.files , undefined , coleccion  );
+    modelo.img = nombre;
+
+    await modelo.save();
+
+    res.json(
+        modelo
+    );*/
+}
+
 module.exports = {
     cargarArchivo,
     actualizarImagen,
-    monstrarImagen
+    monstrarImagen,
+    actualizarImagenCloudinary
 }
